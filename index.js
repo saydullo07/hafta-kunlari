ecb7819cdc62a19e33621627d33f38c404ad9ba6
saydullo07 <ssoibnazarov07@gmail.com>
2024-03-29 09:42:13 +0500

    hafta kunlari

---
+++ index.js
@@ -0,0 +1,32 @@
+let b = prompt()
+let a
+console.log(a = b);
+
+if(a == 1 ){
+console.log( 'Dushanba');
+}
+
+if(a == 2 ){
+    console.log( 'Sishanba');
+    }
+
+    if(a == 3 ){
+        console.log( 'CHorshanba');
+        }
+
+        if(a == 4 ){
+            console.log( 'Payshanba');
+            }
+
+            if(a == 5 ){
+                console.log( 'Juma');
+                }
+
+                if(a == 6 ){
+                    console.log( 'SHanba');
+                    }
+
+                    if(a == 7 ){
+                        console.log( 'Yakshanba');
+                        }
+
